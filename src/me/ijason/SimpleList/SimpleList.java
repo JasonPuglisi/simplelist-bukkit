30e676b28444d3ce5c83dec98ebcd5def23731b7
Jason Puglisi <jason@ijason.me>
2013-10-25 22:31:38 -0400

    Updated plugin check structure.

--- src/me/ijason/SimpleList/SimpleList.java
+++ src/me/ijason/SimpleList/SimpleList.java
@@ -1,22 +1,26 @@
 package me.ijason.SimpleList;
 
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**@author Jason Puglisi
- * @version 1.2.0
+ * @version 1.2.2
  */
 
 public class SimpleList extends JavaPlugin {
-	public static boolean pex = false;
-	public static boolean sc = false;
+	public static Plugin pex;
+	public static Plugin sc;
+	public static Plugin vnp;
 	// Plugin enable
 	public void onEnable() {
+		PluginManager pm = getServer().getPluginManager();
 		getLogger().info("v" + getDescription().getVersion() + " enabled");
 		getCommand("who").setExecutor(new List(this));
 		getCommand("list").setExecutor(new List(this));
 		getCommand("players").setExecutor(new List(this));
-		pex = getServer().getPluginManager().getPlugin("PermissionsEx") != null;
-		sc = getServer().getPluginManager().getPlugin("SupaChat") != null;
+		pex = pm.getPlugin("PermissionsEx");
+		sc = pm.getPlugin("SupaChat");
 	}
 	// Plugin disable
 	public void onDisable() {
@@ -24,10 +28,10 @@ public class SimpleList extends JavaPlugin {
 	}
 	// PermissionsEx Enabled
 	public static boolean pexEnabled() {
-		return pex;
+		return pex != null;
 	}
 	// SupaChat Enabled
 	public static boolean scEnabled() {
-		return sc;
+		return sc != null;
 	}
 }
