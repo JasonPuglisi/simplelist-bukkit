d20561a42e5409f570cce3e4420938dc795988bf
Jason Puglisi <jason@ijason.me>
2013-10-25 22:32:34 -0400

    Added SupaChat prefix support.

--- src/me/ijason/SimpleList/ScInfo.java
+++ src/me/ijason/SimpleList/ScInfo.java
@@ -1,10 +1,17 @@
 package me.ijason.SimpleList;
 
-public class ScInfo {
-	public static String getPrefix() {
-		return "";
-	}
-	public static String getSuffix() {
+import me.javoris767.supachat.SupaChat;
+
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ScInfo extends JavaPlugin {
+	static Plugin supachatplugin = SimpleList.sc;
+	public static String getPrefix(Player player) {
+		String prefix = ((SupaChat) supachatplugin).info.getKey(player, "prefix");
+		if (prefix != null) return ChatColor.translateAlternateColorCodes('&', prefix);
 		return "";
 	}
 }
